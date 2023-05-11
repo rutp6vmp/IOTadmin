b5f3e8db997829c93c86d47f45d939ec0547e38f
danielAA <rutp6vmp@yahoo.com.tw>
2023-05-11 16:58:54 +0800

    Initial commit

--- API/models.py
+++ API/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.db import models
 
 class SensorData(models.Model):
-    time = models.DateTimeField(format="%Y/%m/%d %H:%M:%S")
+    time = models.DateTimeField()
     temperature = models.DecimalField(max_digits=5, decimal_places=2)
     humidity = models.DecimalField(max_digits=5, decimal_places=2)
 
