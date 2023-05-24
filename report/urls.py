6fd97ee0bd139c0eacb34ea46b90b15efbc871fa
danielAA <rutp6vmp@yahoo.com.tw>
2023-05-24 17:14:33 +0800

    Initial commit

--- report/urls.py
+++ report/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
 
-    path('api_imagedata/<str:filename>/', views.image_view, name='image_view'),
+    path('api_imagedata/<int:filename>/', views.image_view, name='image_view'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-
-
-urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    path('dashboard/', views.dashboard_now, name='dashboard'),
-]
->>>>>>> origin/master
--- report/views.py
+++ report/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.conf import settings
+import os
+
+def image_view(request, filename):
+    image_path = os.path.join(settings.MEDIA_ROOT, 'images', filename)
+    with open(image_path, 'rb') as f:
+        image_data = f.read()
+    return HttpResponse(image_data, content_type='image/jpeg')
+def home_view(request):
+    return render(request, 'home.html')
