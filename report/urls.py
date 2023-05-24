4cb982962ab9d80a578a2d9b80b8fdeea5be3faf
danielAA <rutp6vmp@yahoo.com.tw>
2023-05-24 17:37:12 +0800

    Initial commit

--- report/urls.py
+++ report/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home/', views.home_view, name='home'),
 
-    path('api_imagedata/<int:filename>/', views.image_view, name='image_view'),
+    path('api_imagedata/<str:filename>/', views.image_view, name='image_view'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
