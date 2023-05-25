5ab80799c3df8dec4b2c8ec62e803df7d98bfa33
LouisLin511986 <93067887+LouisLin511986@users.noreply.github.com>
2023-05-25 18:51:29 +0800

    Update admin.py
    change some code ...

--- API/admin.py
+++ API/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from API.models import *
 
 class ImageDataAdmin(admin.ModelAdmin):
-    fields=('name_image', 'new_time')
+    fields=('name_image', 'new_time', 'image')
     list_display=('name_image', 'new_time', 'showimages')
-    readonly_fields=('new_time', 'showimages', 'date')
+    readonly_fields=('new_time', 'date')
     list_filter = ('date',)
     search_fields = ['date']
 
