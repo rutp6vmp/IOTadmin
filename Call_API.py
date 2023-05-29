849a586beb5be3a2ee6f7752a21cf0aec34965e8
LouisLin511986 <90818126@gcloud.csu.edu.tw>
2023-05-30 01:12:36 +0800

    01:12

--- Call_API.py
+++ Call_API.py
@@ -1,77 +1,6 @@
-# from datetime import datetime
-# import os
-# import requests
-
-# def upload_images_from_folder(url, folder_path):
-#     # 遍历文件夹中的所有文件
-#     for filename in os.listdir(folder_path):
-#         current_time = datetime.now()
-#         formatted_time = current_time.strftime("%Y%m%d%H%M%S")
-#         # 检查文件是否为图片文件
-#         if filename.endswith(('.jpg', '.jpeg', '.png')):
-#             # 构建图像文件的完整路径
-#             image_path = os.path.join(folder_path, filename)
-
-#             # 打开图像文件
-#             with open(image_path, 'rb') as file:
-#                 # 读取图像数据
-#                 image_data = file.read()
-
-#             # 构建请求参数
-#             payload = {
-#                 'time': formatted_time,
-#                 'name_image': filename
-#             }
-
-#             # 使用元组指定文件名和文件对象
-#             files = {
-#                 'image': (filename, image_data),
-#             }
-
-#             # 发送POST请求
-#             response = requests.post(url, data=payload, files=files)
-
-#             # 打印上传结果
-#             print(f"Uploaded {filename}: {response.status_code}")
-#             # print(response.text)  # 打印完整的响应内容
-
-
-
-
-
-# # api_url = 'http://127.0.0.1:8000/API/upload_image/'
-# api_url = 'http://iotadmin.onrender.com/API/upload_image/'
-# image_folder = './local_folder'
-
-# upload_images_from_folder(api_url, image_folder)
-
 from datetime import datetime
 import os
 import requests
-from PIL import Image
-import math
-
-def resize_image(image_path):
-    # 打开图像文件
-    image = Image.open(image_path)
-
-    # 获取图像原始尺寸
-    width, height = image.size
-
-    # 计算缩放后的尺寸
-    target_width = math.ceil(width*0.7)
-    target_height = math.ceil(height*0.7)
-
-    # 缩放图像
-    resized_image = image.resize((target_width, target_height))
-
-    # 保存缩放后的图像
-    resized_image_path = image_path.replace('.jpg', '_resized.jpg')
-    resized_image_path = resized_image_path.replace('.jpeg', '_resized.jpeg')
-    resized_image_path = resized_image_path.replace('.png', '_resized.png')
-    resized_image.save(resized_image_path)
-
-    return resized_image_path
 
 def upload_images_from_folder(url, folder_path):
     # 遍历文件夹中的所有文件
@@ -82,10 +11,9 @@ def upload_images_from_folder(url, folder_path):
         if filename.endswith(('.jpg', '.jpeg', '.png')):
             # 构建图像文件的完整路径
             image_path = os.path.join(folder_path, filename)
-            # 等比例缩小图像
-            resized_image_path = resize_image(image_path)
-            # 打开缩小后的图像文件
-            with open(resized_image_path, 'rb') as file:
+
+            # 打开图像文件
+            with open(image_path, 'rb') as file:
                 # 读取图像数据
                 image_data = file.read()
 
@@ -105,10 +33,82 @@ def upload_images_from_folder(url, folder_path):
 
             # 打印上传结果
             print(f"Uploaded {filename}: {response.status_code}")
+            # print(response.text)  # 打印完整的响应内容
 
-            # 删除缩小后的图像文件
-            os.remove(resized_image_path)
 
+
+
+
+# api_url = 'http://127.0.0.1:8000/API/upload_image/'
 api_url = 'http://iotadmin.onrender.com/API/upload_image/'
 image_folder = './local_folder'
+
 upload_images_from_folder(api_url, image_folder)
+
+# from datetime import datetime
+# import os
+# import requests
+# from PIL import Image
+# import math
+
+# def resize_image(image_path):
+#     # 打开图像文件
+#     image = Image.open(image_path)
+
+#     # 获取图像原始尺寸
+#     width, height = image.size
+
+#     # 计算缩放后的尺寸
+#     target_width = math.ceil(width*0.4)
+#     target_height = math.ceil(height*0.4)
+
+#     # 缩放图像
+#     resized_image = image.resize((target_width, target_height))
+
+#     # 保存缩放后的图像
+#     resized_image_path = image_path.replace('.jpg', '_resized.jpg')
+#     resized_image_path = resized_image_path.replace('.jpeg', '_resized.jpeg')
+#     resized_image_path = resized_image_path.replace('.png', '_resized.png')
+#     resized_image.save(resized_image_path)
+
+#     return resized_image_path
+
+# def upload_images_from_folder(url, folder_path):
+#     # 遍历文件夹中的所有文件
+#     for filename in os.listdir(folder_path):
+#         current_time = datetime.now()
+#         formatted_time = current_time.strftime("%Y%m%d%H%M%S")
+#         # 检查文件是否为图片文件
+#         if filename.endswith(('.jpg', '.jpeg', '.png')):
+#             # 构建图像文件的完整路径
+#             image_path = os.path.join(folder_path, filename)
+#             # 等比例缩小图像
+#             resized_image_path = resize_image(image_path)
+#             # 打开缩小后的图像文件
+#             with open(resized_image_path, 'rb') as file:
+#                 # 读取图像数据
+#                 image_data = file.read()
+
+#             # 构建请求参数
+#             payload = {
+#                 'time': formatted_time,
+#                 'name_image': filename
+#             }
+
+#             # 使用元组指定文件名和文件对象
+#             files = {
+#                 'image': (filename, image_data),
+#             }
+
+#             # 发送POST请求
+#             response = requests.post(url, data=payload, files=files)
+
+#             # 打印上传结果
+#             print(f"Uploaded {filename}: {response.status_code}")
+
+#             # 删除缩小后的图像文件
+#             os.remove(resized_image_path)
+
+# api_url = 'http://iotadmin.onrender.com/API/upload_image/'
+# image_folder = './local_folder'
+# upload_images_from_folder(api_url, image_folder)
